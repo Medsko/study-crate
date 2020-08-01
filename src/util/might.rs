f3e1b06a07963ec70dbc888587571831fbae6fe4
Melle de Vries <melle@openweb.nl>
2020-08-01 14:05:06 +0200

    Give future Rust adept me something to do

---
+++ src/util/might.rs
@@ -0,0 +1,12 @@
+
+pub struct Print {
+    debug: bool,
+}
+
+impl Print {
+    // TODO: finish this struct/class, which prints only if debug=true (to make functions in this crate,
+    //   typically littered with print statements, perform their stuff silently)
+    //  This idea would be useful, but it involves variadic interfaces and macro definitions...so maybe later.
+
+    // fn println(format: &str, )
+}
